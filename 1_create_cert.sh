7538f47913810322fe1d0aa061328fc3c9b3b8da
Falco Prescher <falcoprescher@googlemail.com>
2020-05-01 16:04:48 +0200

    Changing generation of certificate and key

--- 1_create_cert.sh
+++ 1_create_cert.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:4096 -keyout ./ssl/ca_key.pem -out ./ssl/ca_cert.pem -days 365
-# openssl req -new -key client_key.pem -out client_req.csr
-# openssl x509 -req -in client_req.csr -CA ca_cert.pem -CAkey ca_key.key -CAcreateserial -out client_cert.pem -days 1024 -sha256
-# openssl req -x509 -newkey rsa:4096 -keyout ./ssl/client_key.pem -CA ./ssl/ca_cert.pem -CAkey ./ssl/ca_key.key 
-# -CAcreateserial -out ./
-# ssl/client_cert.pem -days 365
-
+openssl genrsa 4096 > ca_key.pem
+openssl req -new -x509 -nodes -sha1 -days 365 -key ./ssl/ca_key.pem -out ./ssl/ca_cert.pem
