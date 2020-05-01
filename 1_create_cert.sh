b24b95bf9ca3d0c9d2d6445c0350f78bbe52c565
Falco Prescher <falcoprescher@googlemail.com>
2020-05-01 15:32:23 +0200

    Commenting out parameters

--- 1_create_cert.sh
+++ 1_create_cert.sh
@@ -3,6 +3,6 @@ openssl req -x509 -newkey rsa:4096 -keyout ./ssl/ca_key.pem -out ./ssl/ca_cert.p
 # openssl req -new -key client_key.pem -out client_req.csr
 # openssl x509 -req -in client_req.csr -CA ca_cert.pem -CAkey ca_key.key -CAcreateserial -out client_cert.pem -days 1024 -sha256
 # openssl req -x509 -newkey rsa:4096 -keyout ./ssl/client_key.pem -CA ./ssl/ca_cert.pem -CAkey ./ssl/ca_key.key 
--CAcreateserial -out ./
-ssl/client_cert.pem -days 365
+# -CAcreateserial -out ./
+# ssl/client_cert.pem -days 365
 
