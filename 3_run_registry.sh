5fd28b52eb6b5637e6827ea816d5f19dfa669f4b
Falco Prescher <falcoprescher@googlemail.com>
2020-05-01 19:42:36 +0200

    Update 3_run_registry.sh

--- 3_run_registry.sh
+++ 3_run_registry.sh
@@ -8,7 +8,8 @@ docker run -d \
   -e REGISTRY_AUTH=htpasswd \
   -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-  -e REGISTRY_HTTP_ADDR=0.0.0.0:9001 \
+  # Alternative Port Binding
+  # -e REGISTRY_HTTP_ADDR=0.0.0.0:9001 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/ssl/ca_cert.pem \
   -e REGISTRY_HTTP_TLS_KEY=/ssl/ca_key.pem \
   -p 9001:443 \
