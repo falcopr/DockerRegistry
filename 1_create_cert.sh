8f96c30da49b670dc29a4e237f39e2184ab5844e
Falco Prescher <falcoprescher@googlemail.com>
2020-05-14 18:02:45 +0200

    Update 1_create_cert.sh

--- 1_create_cert.sh
+++ 1_create_cert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 openssl genrsa 4096 > ca_key.pem
-openssl req -new -x509 -nodes -sha1 -days 365 -key ./ssl/ca_key.pem -out ./ssl/ca_cert.pem
+openssl req -new -x509 -nodes -sha256 -days 365 -key ./ssl/ca_key.pem -out ./ssl/ca_cert.pem
