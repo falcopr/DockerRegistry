a34cf359025cd93a4117fafb8535a3b1e45ea877
Falco P <1677412+falcopr@users.noreply.github.com>
2021-02-14 19:26:35 +0100

    Alternative htpasswd generation

--- 2_create_htpasswd.sh
+++ 2_create_htpasswd.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 docker run --rm --entrypoint htpasswd registry -Bbn username password > ./auth/htpasswd
+
+# The new registry version's htpasswd is faulty
+# Use this for generation temporary
+# docker run --rm -ti xmartlabs/htpasswd <username> <password> > htpasswd
