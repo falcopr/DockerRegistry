0f3af9c1bdbbd6e467e241a0db361fee410f3257
Falco P <1677412+falcopr@users.noreply.github.com>
2021-02-14 12:00:18 +0100

    Using precreated docker volume

--- 3_run_registry.sh
+++ 3_run_registry.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+docker volume create docker_registry_data
+
 docker run -d \
   --restart=always \
-  --name registry \
+  --name docker_registry \
   -v `pwd`/auth:/auth \
   -v `pwd`/ssl:/ssl \
-  -v `pwd`/registry:/var/lib/registry \
+  -v docker_registry_data:/var/lib/registry\
   -e REGISTRY_AUTH=htpasswd \
   -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-  # Alternative Port Binding
-  # -e REGISTRY_HTTP_ADDR=0.0.0.0:9001 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/ssl/ca_cert.pem \
   -e REGISTRY_HTTP_TLS_KEY=/ssl/ca_key.pem \
   -p 9001:443 \
   registry
 
+# Alternative Port Binding
+# -e REGISTRY_HTTP_ADDR=0.0.0.0:9001
+# Binding to local registry folder
+# -v `pwd`/registry:/var/lib/registry \
