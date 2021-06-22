b5dac01203453b8a36e456aacd33e1561d179c78
Falco P <1677412+falcopr@users.noreply.github.com>
2021-06-22 14:16:17 +0200

    Update 2_create_htpasswd.sh

--- 2_create_htpasswd.sh
+++ 2_create_htpasswd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --rm --entrypoint htpasswd registry -Bbn username password > ./auth/htpasswd
+docker run --rm --entrypoint htpasswd httpd -Bbn username password > ./auth/htpasswd
 
 # The new registry version's htpasswd is faulty
 # Use this for generation temporary
